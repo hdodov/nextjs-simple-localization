2f298e001343c0b96a392a5cb79b0e8dffdd4c1d
Hristiyan Dodov <h.dodov@gmail.com>
2024-08-04 00:22:37 +0300

    Add alternates to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,18 @@
-import { defaultLocale, locales } from "@/config";
-import { baseUrlLocalized } from "@/utilities/l10n";
+import { defaultLocale, locales, pages } from "@/config";
+import { pageUrlLocalized } from "@/utilities/l10n";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return locales.map((locale) => ({
-    url: baseUrlLocalized(locale),
+  return pages.map((p) => ({
+    url: pageUrlLocalized(defaultLocale, p),
     lastModified: new Date(),
-    priority: locale === defaultLocale ? 1 : 0.5,
+    priority: p === "home" ? 1 : 0.5,
+    alternates: {
+      languages: Object.fromEntries(
+        locales
+          .filter((l) => l !== defaultLocale)
+          .map((l) => [l, pageUrlLocalized(l, p)])
+      ),
+    },
   }));
 }
--- utilities/l10n.ts
+++ utilities/l10n.ts
@@ -20,9 +20,12 @@ export const basePathLocalized = (locale?: Locale) =>
   `/${locale === defaultLocale ? "" : `${locale}/`}`;
 
 export const pagePathLocalized = (locale: Locale, page: Page) =>
-  `${basePathLocalized(locale)}${page === "home" ? "" : page}`;
+  `${basePathLocalized(locale)}${page === "home" ? "" : page}/`;
 
 export const baseUrlLocalized = (locale?: Locale) =>
   `${baseUrl}${locale === defaultLocale ? "" : `/${locale}`}`;
+
+export const pageUrlLocalized = (locale: Locale, page: Page) =>
+  `${baseUrlLocalized(locale)}${page === "home" ? "" : `/${page}`}`;
 
 export const state = { locale: defaultLocale };
